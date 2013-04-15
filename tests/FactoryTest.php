c03d2e916f74c4603ce2e2086e7918e223aee521
edvinaskrucas <edv.krucas@gmail.com>
2013-04-16 01:14:00 +0300

    Added factory tests

---
+++ tests/FactoryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use Mockery as m;
+
+class FactoryTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+
+    public function testFactoryCreation()
+    {
+        $f = $this->getFactory();
+
+        $this->assertInstanceOf('Krucas\Service\Validator\Factory', $f);
+    }
+
+
+    public function testGetIlluminateFactory()
+    {
+        $f = $this->getFactory();
+
+        $this->assertInstanceOf('Illuminate\Validation\Factory', $f->getIlluminateValidationFactory());
+    }
+
+
+    public function testCreateValidatorService()
+    {
+        $f = $this->getFactory();
+
+        $mock = m::mock('Krucas\Service\Validator\Contracts\ValidatableInterface');
+
+        $this->assertInstanceOf('Krucas\Service\Validator\Validator', $f->make($mock));
+    }
+
+
+    public function getFactory()
+    {
+        return new \Krucas\Service\Validator\Factory(
+            m::mock('Illuminate\Validation\Factory')
+        );
+    }
+}
